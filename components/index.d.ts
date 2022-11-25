0f50689b65fcb89fdeb1616ee66c62c6547dbf74
HyeongChang Lee <hc9904@hanyang.ac.kr>
2022-11-26 04:38:44 +0900

    v1.0.15
    update Type

--- components/index.d.ts
+++ components/index.d.ts
@@ -1,1 +1,11 @@
-declare module '@anoldstory/ago-react'
+import React from 'react';
+
+export interface SearchBar {
+  apiKey: string;
+  targetIndexName: string;
+  targetField: string;
+}
+
+declare module '@anoldstory/ago-react' {
+  export { SearchBar };
+}
