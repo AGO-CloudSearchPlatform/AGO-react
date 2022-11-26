a55faa7358a863486c41af4762c2cc7f49237c02
HyeongChang Lee <hc9904@hanyang.ac.kr>
2022-11-26 12:38:32 +0900

    1.0.17

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 const SearchBarExample = ({ apiKey, targetField, targetIndexName }) => {
   const [searchContent, setSearchContent] = useState("");
@@ -73,15 +74,18 @@ const SearchBarExample = ({ apiKey, targetField, targetIndexName }) => {
   };
 
   return (
-    <div className="w-[600px] p-2">
-      <input
-        type="text"
-        className="w-full p-3"
-        onChange={handleChange}
-        value={searchContent}
-        onKeyPress={handleKeyPress}
-      />
-
+    <div className="w-[600px]">
+      <div className="w-[550px] p-2 flex items-center rounded-md border bg-white">
+        <input
+          type="text"
+          className="w-full p-3 focus:placeholder:text-transparent focus:outline-none border-none "
+          onChange={handleChange}
+          value={searchContent}
+          onKeyPress={handleKeyPress}
+          placeholder="무엇을 찾고 싶으신가요?"
+        />
+        <BiSearchAlt2 className="bg-white" size={26} />
+      </div>
       {suggestedItems.length !== 0 && (
         <div className="bg-white w-full space-y-3 p-4 text-gray-400">
           {suggestedItems.map((tmp, index) => (
@@ -103,7 +107,7 @@ const SearchBarExample = ({ apiKey, targetField, targetIndexName }) => {
         {searchResult.map((searchItem) => (
           <div className="shadow-lg p-4 space-y-2">
             {Object.keys(searchItem.source).map((field) => (
-              <div>
+              <div className="p-2 border">
                 {field} : {searchItem.source[field]}
               </div>
             ))}
